dc8e36a48341adf38e61b450f90171e58d056361
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2025-09-06 23:43:47 -0700

    Initial work on FPGA APB identification core etc

--- trion-t20-bga256/stm32h750-expansion/rtl/Peripherals_APB1.sv
+++ trion-t20-bga256/stm32h750-expansion/rtl/Peripherals_APB1.sv
@@ -29,132 +29,53 @@
 *                                                                                                                      *
 ***********************************************************************************************************************/
 
-module top(
-	input wire			clk_50mhz,
+module Peripherals_APB1(
 
-	//FMC interface
-	input wire			fmc_clk,
-	output wire			fmc_nwait,
-	input wire			fmc_noe,
-	inout wire[15:0]	fmc_ad,
-	input wire			fmc_nwe,
-	input wire[1:0]		fmc_nbl,
-	input wire			fmc_nl_nadv,
-	input wire[6:0]		fmc_a_hi,
-	input wire			fmc_ne1,
+	//Upstream bus interface
+	APB.completer		apb,
 
-	output wire[7:0]	led_n
+	//GPIO LEDs
+	output wire[7:0]	led
 );
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Output port glue
+	// Root interconnect bridge (c000_0000, 1 kB / 0x400 segments per peripheral)
 
-	//Invert active-low LED signals
-	wire[7:0]		led_int;
-	assign led_n = ~led_int;
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// Reset logic clocked by the external 50 MHz clock (real designs will need external reset input)
-
-	logic		pll_rst_n	= 0;
-	logic[7:0]	rst_count	= 1;
-
-	always_ff @(posedge clk_50mhz) begin
-		if(!pll_rst_n) begin
-			rst_count	<= rst_count + 8'h1;
-			if(rst_count == 0)
-				pll_rst_n	<= 1;
-		end
-	end
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// PLL
-
-	wire	pll_lock;
-	wire	clk_fb;
-	wire	pclk;
-	EFX_PLL_V2 #(
-		.N(1),				//pre divide = 1, so xx MHz at the PFD (range 10 - 100)
-		.M(1),				//multiplier 16, so xx MHz at the VCO (range 500 - 1600 for internal FB, 500 - 3600 for other)
-		.O(8),				//post divider between VCO and all outputs
-							//(must be 2 or higher if multiple outputs active)
-		.CLKOUT0_DIV(4),	//60 MHz output for feedback alignment
-		.CLKOUT1_DIV(4),	//60 MHz output but with adjustable phase
-		.CLKOUT2_DIV(128),	//not used, slow to save power
-		.CLKOUT0_PHASE(0),
-		.CLKOUT1_PHASE(0),
-		.CLKOUT2_PHASE(0),
-		.FEEDBACK_CLK("CLK0"),
-		.FEEDBACK_MODE("LOCAL"),
-		.REFCLK_FREQ(62.5)	//refclk frequency in MHz
-	) pll (
-		.CLKIN({3'b0, fmc_clk}),
-		.CLKSEL(2'b00),
-		.RSTN(pll_rst_n),
-		.FBK(pclk),
-		.CLKOUT0(pclk),
-		.CLKOUT1(),
-		.CLKOUT2(),
-		.LOCKED(pll_lock)
-	);
-
-	//Synchronize PLL lock signal into the APB clock domain
-	wire	pll_lock_sync;
-	ThreeStageSynchronizer #(
-		.IN_REG(0)
-	) sync_pll_lock(
-		.clk_in(pclk),
-		.din(pll_lock),
-		.clk_out(pclk),
-		.dout(pll_lock_sync));
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// APB bridge
-
-	APB #(.DATA_WIDTH(32), .ADDR_WIDTH(27), .USER_WIDTH(0)) fmc_apb();
-	APB #(.DATA_WIDTH(64), .ADDR_WIDTH(27), .USER_WIDTH(0)) fmc_apb64();
-
-	FMC_APBBridge_EFX bridge(
-		.apb_x32(fmc_apb),
-		.apb_x64(fmc_apb64),
-
-		.pll_lock(pll_lock),
-
-		.fmc_clk(pclk),
-		.fmc_nwait(fmc_nwait),
-		.fmc_noe(fmc_noe),
-		.fmc_ad(fmc_ad),
-		.fmc_nwe(fmc_nwe),
-		.fmc_nbl(fmc_nbl),
-		.fmc_nl_nadv(fmc_nl_nadv),
-		.fmc_a_hi({3'b0, fmc_a_hi}),
-		.fmc_cs_n(fmc_ne1)
-	);
-
-	//Root APB interconnect
-	//Two 16-bit bus segments at 0xc000_0000 (APB1) and c001_0000 (APB2) for core peripherals
-	APB #(.DATA_WIDTH(32), .ADDR_WIDTH(16), .USER_WIDTH(0)) rootAPB[1:0]();
+	//APB1 segment
+	localparam NUM_PERIPHERALS	= 2;
+	localparam BLOCK_SIZE		= 32'h400;
+	localparam ADDR_WIDTH		= $clog2(BLOCK_SIZE);
+	APB #(.DATA_WIDTH(32), .ADDR_WIDTH(ADDR_WIDTH), .USER_WIDTH(0)) apb1[NUM_PERIPHERALS-1:0]();
 	APBBridge #(
 		.BASE_ADDR(32'h0000_0000),
-		.BLOCK_SIZE(32'h1_0000),
-		.NUM_PORTS(2)
-	) root_bridge (
-		.upstream(fmc_apb),
-		.downstream(rootAPB)
+		.BLOCK_SIZE(BLOCK_SIZE),
+		.NUM_PORTS(NUM_PERIPHERALS)
+	) bridge (
+		.upstream(apb),
+		.downstream(apb1)
 	);
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// APB1 (0xc000_0000 - c000_ffff)
+	// Device information (c000_0000)
 
-	Peripherals_APB1 apb1(
-		.apb(rootAPB[0]),
-		.led(led_int)
+	//Trion has no way to determine runtime identity of the part, so we have to hard code here for compatibility
+	APB_DeviceInfo_Trion #(
+		.IDCODE(32'h00210a79)	//T20F256
+	) devinfo (
+		.apb(apb1[0])
 	);
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// APB2 (0xc001_0000 - c001_ffff) eventually
+	// LED GPIO (c000_0400)
 
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// TODO: whatever lives on apb64
+	wire[31:0]	gpio_out;
+	APB_GPIO gpio(
+		.apb(apb1[1]),
+		.gpio_out(gpio_out),
+		.gpio_in(gpio_out),	//loop back for readback
+		.gpio_tris()
+	);
+
+	assign led = gpio_out[7:0];
 
 endmodule
